18bab2fe3d6684f82dec81b8b985ebc814bbdbcb
KhanaTechnologies <137198849+KhanaTechnologies@users.noreply.github.com>
2024-06-22 09:56:53 +0200

    Update product.js

--- models/product.js
+++ models/product.js
@@ -15,7 +15,7 @@ const productSchema = new Schema({
   rating: { type: Number, default: 0 },
   numReviews: { type: Number, default: 0 },
   isFeatured: { type: Boolean, default: false },
-  client: { type: String, required: true }, // Assuming client ID is stored as a string
+  clientID: { type: String, required: true }, // Assuming client ID is stored as a string
   dateCreated: { type: Date, default: Date.now },
   sizes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Size', required: true }], // Array of objects representing size and quantity
 });
