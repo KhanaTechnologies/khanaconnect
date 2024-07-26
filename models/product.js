acf501604d8f93a007e1fc219107e0d175197445
KhanaTechnologies <137198849+KhanaTechnologies@users.noreply.github.com>
2024-07-26 12:53:04 +0200

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Variant Schema
+const variantSchema = new Schema({
+  value: { type: String, required: true },
+  price: { type: Number, required: true, default: 0 },
+  quantity: { type: Number, required: true, default: 0 }
+});
 
 // Product Schema
 const productSchema = new Schema({
@@ -15,9 +21,13 @@ const productSchema = new Schema({
   rating: { type: Number, default: 0 },
   numReviews: { type: Number, default: 0 },
   isFeatured: { type: Boolean, default: false },
-  clientID: { type: String, required: true }, // Assuming client ID is stored as a string
+  client: { type: String, required: true }, // Assuming client ID is stored as a string
   dateCreated: { type: Date, default: Date.now },
-  sizes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Size', required: true }], // Array of objects representing size and quantity
+  sizes: [variantSchema], // Array of objects representing size variants
+  colors: [variantSchema], // Array of objects representing color variants
+  materials: [variantSchema], // Array of objects representing material variants
+  styles: [variantSchema], // Array of objects representing style variants
+  titles: [variantSchema] // Array of objects representing title variants
 });
 
 // Add a virtual 'id' field to make it compatible with JSON responses
