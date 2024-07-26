3885a6fe1ad4d9432285e8a408cf1cd4068e4f12
KhanaTechnologies <137198849+KhanaTechnologies@users.noreply.github.com>
2024-07-26 15:42:37 +0200

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -1,68 +1,88 @@
 const nodemailer = require('nodemailer');
+const { OrderItem } = require('../models/orderItem');
+const Product = require('../models/product');
 
-// Function to send a verification email
-async function sendVerificationEmail(email, verificationToken, bEmail, BEPass) {
+// Function to send order confirmation email
+async function sendOrderConfirmationEmail(email, orderItems, bEmail, BEPass, shipping) {
     // Create a nodemailer transporter
     const transporter = nodemailer.createTransport({
-    host: 'smtpout.secureserver.net', // GoDaddy SMTP server
-    port: 465, // GoDaddy SMTP port (465 or 587)
-    secure: true, // true for 465, false for other ports
-    auth: {
-        user: bEmail, // Your GoDaddy email address
-        pass: BEPass // Your GoDaddy email password
+        host: 'smtpout.secureserver.net', // GoDaddy SMTP server
+        port: 465, // GoDaddy SMTP port (465 or 587)
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: bEmail, // Your GoDaddy email address
+            pass: BEPass // Your GoDaddy email password
+        }
+    });
+
+    try {
+        console.log(orderItems);
+
+        // Function to populate the order items
+        async function populateOrderItems(orderItems) {
+            return Promise.all(orderItems.map(async item => {
+                // Populate the product field
+                const populatedItem = await Product.findById(item.product);
+                return {
+                    ...item,
+                    product: populatedItem
+                };
+            }));
+        }
+
+        // Populate the order items
+        const populatedOrderItems = await populateOrderItems(orderItems);
+        console.log(populatedOrderItems);
+
+        // Create the order items HTML
+        const orderItemsHtml = populatedOrderItems.map(item => `
+            <tr>
+                <td><img src="${item.product.images[0]}" alt="${item.product.productName}" style="height: 100px;"></td>
+                <td>${item.product.productName}</td>
+                <td>${item.quantity}</td>
+               
+                <td>R${item.product.price}</td>
+                <td>R${item.quantity * item.product.price}</td>
+            </tr>
+        `).join('');
+
+        // Calculate the total price
+        const totalPrice = populatedOrderItems.reduce((total, item) => total + (item.quantity * item.product.price), 0);
+
+        // Send the email
+        await transporter.sendMail({
+            from: bEmail, // Your GoDaddy email address
+            to: email,
+            subject: 'Order Confirmation',
+            html: `
+                <p>Hi,</p>
+                <p>Thank you for your order. Here are the details:</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Product</th>
+                            <th>Quantity</th>
+                            
+                            <th>Price</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${orderItemsHtml}
+                    </tbody>
+                </table>
+                <p> Subtotal:	R${totalPrice}</p>
+                <p>Shipping: R${shipping}</p>
+                <p>Total Price: R${totalPrice + shipping}</p>
+            `
+        });
+
+        console.log('Order confirmation email sent successfully');
+    } catch (error) {
+        console.error('Error sending order confirmation email:', error);
+        throw error; // Throw error to handle it in the calling function
     }
-    });
-
-
-  try {
-    // Send email
-    await transporter.sendMail({
-      from: bEmail, // Your GoDaddy email address
-      to: email,
-      subject: 'Verify your email address',
-      html: `
-        <p>Please click the following link to verify your email address:</p>
-        <a href="http://localhost:3000/api/v1/customer/verify?token=${verificationToken}">Verify Email</a>
-      `
-    });
-
-    console.log('Verification email sent successfully');
-  } catch (error) {
-    console.error('Error sending verification email:', error);
-    throw error; // Throw error to handle it in the calling function
-  }
 }
 
-
-async function orderItemProcessed(email, bEmail, BEPass){
-   // Create a nodemailer transporter
-   const transporter = nodemailer.createTransport({
-    host: 'smtpout.secureserver.net', // GoDaddy SMTP server
-    port: 465, // GoDaddy SMTP port (465 or 587)
-    secure: true, // true for 465, false for other ports
-    auth: {
-        user: bEmail, // Your GoDaddy email address
-        pass: BEPass // Your GoDaddy email password
-    }
-    });
-
-
-  try {
-    // Send email
-    await transporter.sendMail({
-      from: bEmail, // Your GoDaddy email address
-      to: email,
-      subject: 'Order Processed',
-      html: `
-        <p>Please note that your order has been processed!</p>
-      `
-    });
-
-    console.log('Email Update sent successfully');
-  } catch (error) {
-    console.error('Error sending email  update:', error);
-    throw error; // Throw error to handle it in the calling function
-  }
-}
-
-module.exports = { sendVerificationEmail,orderItemProcessed};
+module.exports = { sendOrderConfirmationEmail };
