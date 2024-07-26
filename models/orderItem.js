3abacc3e3e5085c164cb55319ff55b3556a74a20
KhanaTechnologies <137198849+KhanaTechnologies@users.noreply.github.com>
2024-07-26 13:56:38 +0200

    Update orderItem.js

--- models/orderItem.js
+++ models/orderItem.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Variant Schema
+const variantSchema = new Schema({
+    value: { type: String, required: true },
+    price: { type: Number, required: true, default: 0 },
+    quantity: { type: Number, required: true, default: 0 }
+  });
+  
 
 const orderItemSchema = mongoose.Schema({
     quantity: {
@@ -9,10 +18,11 @@ const orderItemSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Product'
     },
-    size: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Size'
-    }
+    sizes: [variantSchema], // Array of objects representing size variants
+    colors: [variantSchema], // Array of objects representing color variants
+    materials: [variantSchema], // Array of objects representing material variants
+    styles: [variantSchema], // Array of objects representing style variants
+    titles: [variantSchema] // Array of objects representing title variants
 })
 
 orderItemSchema.virtual('id').get(function (){return this._id.toHexString();});
