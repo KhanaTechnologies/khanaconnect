19423e5ffc1498eae044d3bc34bb6beec2edcc97
KhanaTechnologies <137198849+KhanaTechnologies@users.noreply.github.com>
2026-01-30 23:15:34 +0200

    Update gaClient.js

--- helpers/gaClient.js
+++ helpers/gaClient.js
@@ -1,7 +1,7 @@
 const { BetaAnalyticsDataClient } = require("@google-analytics/data");
 
 const gaClient = new BetaAnalyticsDataClient({
-  keyFilename: "/etc/secrets/khanaconnect-484716-842b641963bf.json"
+  credentials: JSON.parse(process.env.GA_SERVICE_ACCOUNT_JSON)
 });
 
 module.exports = gaClient;
